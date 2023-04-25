cd0e444d681ff584926512ed013fd566cf5fe5d5
Leo Kenny <94982692+Faoladh@users.noreply.github.com>
2023-04-25 20:27:16 +0100

    the game

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))] 
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [Tooltip("Adds 1 point to hit points when it dies")]
+    [SerializeField] int difficultyRamp = 1;
+    int currentHitPoints = 0;
+    
+    Enemy enemy;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if(currentHitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
+            enemy.RewardGold();
+        }
+    }
+}
